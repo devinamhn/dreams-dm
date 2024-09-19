7cf9715fed931ab789f07d798b3cda4a0c014c7e
devinamhn <devinamhn@gmail.com>
2024-09-19 17:29:05 -0400

    dataset file

---
+++ dreamsdm/datasets/wdmgal.py
@@ -0,0 +1,31 @@
+import os
+import pandas as pd
+from torch.utils.data import Dataset
+from torchvision.io import read_image 
+
+class WDMGalaxiesDataset(Dataset):
+    """
+    
+    """
+    
+    def __init__(self, csv_file, img_dir, transform = None,
+                 target_transform = None):
+        self.labels = pd.read_csv(csv_file)
+        self.img_dir = img_dir
+        self.transform = transform 
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.labels.iloc[idx,0])
+        #would be better to have imgs in jpeg/png, can use read_image directly
+
+        image = read_image(img_path)
+        label = self.labels.iloc[idx, 1]
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
