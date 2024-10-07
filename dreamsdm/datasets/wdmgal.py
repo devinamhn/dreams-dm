56668723755e8211f12db468ee5edd35bdcbfc3f
devinamhn <devinamhn@gmail.com>
2024-10-07 14:39:59 -0400

    Update dataset class

--- dreamsdm/datasets/wdmgal.py
+++ dreamsdm/datasets/wdmgal.py
@@ -6,25 +6,53 @@ from pathlib import Path
 
 class WDMGalaxiesDataset(Dataset):
     """
-    
+    file_path: path to hdf file
+    dataset_name: specify train or test datasets
+    transform, target_transforms: optional transforms
     """
     
-    def __init__(self, csv_file, img_dir, transform = None,
+    def __init__(self, file_path, dataset_name, transform = None,
                  target_transform = None):
-        self.labels = pd.read_csv(csv_file)
-        self.img_dir = img_dir
+        self.file_path = file_path
+        self.dataset_name = dataset_name
         self.transform = transform 
         self.target_transform = target_transform
+        self.length = None
+        self._idx_to_name = {} #data_dict 
+
+        with h5py.File(self.file_path, 'r') as hf:
+            for gname, group in hf.items():
+                if gname == self.dataset_name:
+                    sample_id_idx = 0
+                    for sim_id, dd in (group.items()):
+                        for Mgas_id, ee in enumerate(dd.items()):
+                            self._idx_to_name[sample_id_idx] = [sim_id, ee[0]]
+                            sample_id_idx+=1 
+                    self.length = sample_id_idx
+        print(self._idx_to_name)
 
     def __len__(self):
-        return len(self.labels)
+        assert self.length is not None
+        return self.length
+    
+    def _open_hdf5(self):
+        self._hf = h5py.File(self.file_path, 'r')
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.labels.iloc[idx,0])
-        image = read_image(img_path)
-        label = self.labels.iloc[idx, 1]
+        # if (torch.is_tensor(idx)):
+        #     idx = idx.tolist()
+        if not hasattr(self, '_hf'):
+            self._open_hdf5()
+
+        sim_id, Mgas_id = self._idx_to_name[idx]
+        data = self._hf[self.dataset_name][sim_id][Mgas_id]
+        image = np.array(data)
+        label = torch.tensor(data.attrs['WDM'])
 
         if self.transform:
             image = self.transform(image)
+
+        # if self.target_transform:
+            # label = self.target_transform(label)
 
         return image, label
