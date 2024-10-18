44fd4222f00b2c0b9e8b6c15e06d645ec135e479
devinamhn <devinamhn@gmail.com>
2024-10-18 18:59:49 -0400

    update

--- dreamsdm/utils.py
+++ dreamsdm/utils.py
@@ -1,4 +1,20 @@
 from pathlib import Path
+import torch
+# from torchvision.models import resnet18, ResNet18_Weights
+import torch.nn as nn
+
+import h5py
+from pathlib import Path
+import pandas as pd
+import os
+
+import numpy as np
+from pathlib import Path
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
 
 class Path_Handler:
     """Handle and generate paths in project directory"""
@@ -46,3 +62,32 @@ def create_path(path):
     if not Path.exists(path):
         Path.mkdir(path)
 
+def train(model, optimizer, criterion, train_loader, device):
+
+    running_loss = 0.0
+    for i, (x_train, y_train) in enumerate(train_loader):
+        x_train, y_train = x_train.to(device), y_train.to(device)
+        optimizer.zero_grad()
+        outputs = model(x_train).flatten()
+        # print(outputs, y_train)
+        loss = criterion(outputs.to(torch.double), y_train)
+        # print(outputs.to(torch.double).dtype, y_train.dtype)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+ 
+    return running_loss/(i+1)
+
+def validate(model, criterion, validation_loader, device):
+    running_loss = 0.0
+    # running_error = 0.0
+    # enable_dropout(model)
+    with torch.no_grad():
+        for i, (x_val, y_val) in enumerate(validation_loader):
+            x_val, y_val = x_val.to(device), y_val.to(device)
+            outputs = model(x_val).flatten()
+            # print(outputs, y_val)
+            loss = criterion(outputs.to(torch.double), y_val)
+            running_loss += loss.item()
+            
+    return running_loss/(i+1)
