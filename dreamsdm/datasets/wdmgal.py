73a1635e77581e055b5cf40f754a38b085011203
devinamhn <devinamhn@gmail.com>
2024-10-18 19:00:06 -0400

    updated dataset class

--- dreamsdm/datasets/wdmgal.py
+++ dreamsdm/datasets/wdmgal.py
@@ -3,56 +3,67 @@ import pandas as pd
 from torch.utils.data import Dataset
 from torchvision.io import read_image 
 from pathlib import Path
+import h5py
+import numpy as np
+import torch
+import h5py
+from torchvision import transforms
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
 
-class WDMGalaxiesDataset(Dataset):
+
+
+class WDMGalaxies(Dataset):
     """
     file_path: path to hdf file
     dataset_name: specify train or test datasets
     transform, target_transforms: optional transforms
     """
     
-    def __init__(self, file_path, dataset_name, transform = None,
+    def __init__(self, file_path, n_files, n_gal,
+                 # dataset_name, 
+                 transform = None,
                  target_transform = None):
-        self.file_path = file_path
-        self.dataset_name = dataset_name
+        # self.file_path = file_path
         self.transform = transform 
         self.target_transform = target_transform
-        self.length = None
-        self._idx_to_name = {} #data_dict 
 
-        with h5py.File(self.file_path, 'r') as hf:
-            for gname, group in hf.items():
-                if gname == self.dataset_name:
-                    sample_id_idx = 0
-                    for sim_id, dd in (group.items()):
-                        for Mgas_id, ee in enumerate(dd.items()):
-                            self._idx_to_name[sample_id_idx] = [sim_id, ee[0]]
-                            sample_id_idx+=1 
-                    self.length = sample_id_idx
-        print(self._idx_to_name)
+        length_total = 0
+        data = np.zeros((n_files*n_gal, 1, 512, 512), dtype=np.float32)
+        wdm = np.zeros((n_files*n_gal))
+        
+        for n_file in range(n_files):
+            
+            file_path_ = file_path + str(n_file) + '.hdf5'
+            _hf = h5py.File(file_path_, 'r')
+            
+            hf_keys = list(_hf.keys())
+            length= len(hf_keys)
+            
+            for i in range(length):
+                data[i+n_file*n_gal::] = _hf[hf_keys[i]]
+                wdm[i+n_file*n_gal::] =  _hf[hf_keys[i]].attrs['WDM']
+                length_total+=1
+                if(i== n_gal-1):
+                    break
+            _hf.close()
 
+        self.data = torch.tensor(data)
+        self.labels = torch.tensor(wdm)
+        self.length = length_total
+        
     def __len__(self):
         assert self.length is not None
         return self.length
-    
-    def _open_hdf5(self):
-        self._hf = h5py.File(self.file_path, 'r')
 
     def __getitem__(self, idx):
-        # if (torch.is_tensor(idx)):
-        #     idx = idx.tolist()
-        if not hasattr(self, '_hf'):
-            self._open_hdf5()
-
-        sim_id, Mgas_id = self._idx_to_name[idx]
-        data = self._hf[self.dataset_name][sim_id][Mgas_id]
-        image = np.array(data)
-        label = torch.tensor(data.attrs['WDM'])
-
+        image = self.data[idx]
+        label = self.labels[idx]
         if self.transform:
-            image = self.transform(image)
-
+            image = self.transform(data)
         # if self.target_transform:
             # label = self.target_transform(label)
-
+        
         return image, label
+
+
