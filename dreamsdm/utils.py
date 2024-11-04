b6eacdf58d0e8674f18465bcde7b4d85530ed6c7
devinamhn <devinamhn@gmail.com>
2024-11-04 18:35:45 -0500

    added train/eval functions for multioutput regression model

--- dreamsdm/utils.py
+++ dreamsdm/utils.py
@@ -7,7 +7,7 @@ import h5py
 from pathlib import Path
 import pandas as pd
 import os
-
+import yaml
 import numpy as np
 from pathlib import Path
 from torch.utils.data import Dataset
@@ -15,6 +15,7 @@ from torchvision import transforms
 
 from torchvision.transforms import ToTensor
 from torch.utils.data import DataLoader
+from sklearn.metrics import r2_score
 
 class Path_Handler:
     """Handle and generate paths in project directory"""
@@ -65,29 +66,78 @@ def create_path(path):
 def train(model, optimizer, criterion, train_loader, device):
 
     running_loss = 0.0
+    y_pred = []
+    y_true = []
     for i, (x_train, y_train) in enumerate(train_loader):
         x_train, y_train = x_train.to(device), y_train.to(device)
         optimizer.zero_grad()
         outputs = model(x_train).flatten()
-        # print(outputs, y_train)
+        y_pred.append(outputs.to(torch.double))
+        y_true.append(y_train)
         loss = criterion(outputs.to(torch.double), y_train)
         # print(outputs.to(torch.double).dtype, y_train.dtype)
         loss.backward()
         optimizer.step()
         running_loss += loss.item()
+
+    r2_ = r2_score(torch.cat(y_true).cpu().detach().numpy(), torch.cat(y_pred).cpu().detach().numpy())
  
-    return running_loss/(i+1)
+    return running_loss/(i+1), r2_
 
 def validate(model, criterion, validation_loader, device):
     running_loss = 0.0
-    # running_error = 0.0
-    # enable_dropout(model)
+    y_pred = []
+    y_true = []
     with torch.no_grad():
         for i, (x_val, y_val) in enumerate(validation_loader):
             x_val, y_val = x_val.to(device), y_val.to(device)
             outputs = model(x_val).flatten()
-            # print(outputs, y_val)
+            y_pred.append(outputs.to(torch.double))
+            y_true.append(y_val)
             loss = criterion(outputs.to(torch.double), y_val)
             running_loss += loss.item()
-            
-    return running_loss/(i+1)
+    r2_ = r2_score(torch.cat(y_true).cpu().detach().numpy(), torch.cat(y_pred).cpu().detach().numpy())
+    return running_loss/(i+1), r2_
+
+# Load configuration from a YAML file
+def load_config(config_path):
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
+
+def train_all(model, optimizer, criterion, train_loader, device):
+
+    running_loss = 0.0
+    y_pred = []
+    y_true = []
+    for i, (x_train, y_train) in enumerate(train_loader):
+        x_train, y_train = x_train.to(device), y_train.to(device)
+        optimizer.zero_grad()
+        outputs = model(x_train)
+        y_pred.append(outputs.to(torch.double))
+        y_true.append(y_train)
+        loss = criterion(outputs.to(torch.double), y_train)
+        # print(outputs.to(torch.double).dtype, y_train.dtype)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+
+    r2_ = r2_score(torch.cat(y_true).cpu().detach().numpy(), torch.cat(y_pred).cpu().detach().numpy(), multioutput = 'raw_values' )
+ 
+    return running_loss/(i+1), r2_
+
+def validate_all(model, criterion, validation_loader, device):
+    running_loss = 0.0
+    y_pred = []
+    y_true = []
+    with torch.no_grad():
+        for i, (x_val, y_val) in enumerate(validation_loader):
+            x_val, y_val = x_val.to(device), y_val.to(device)
+            outputs = model(x_val)
+            y_pred.append(outputs.to(torch.double))
+            y_true.append(y_val)
+            loss = criterion(outputs.to(torch.double), y_val)
+            running_loss += loss.item()
+    r2_ = r2_score(torch.cat(y_true).cpu().detach().numpy(), torch.cat(y_pred).cpu().detach().numpy(), multioutput = 'raw_values')
+    return running_loss/(i+1), r2_
+
