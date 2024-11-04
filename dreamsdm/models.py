aeff1aaea0e45b34ae0128aec46d2fc7a1e907c7
devinamhn <devinamhn@gmail.com>
2024-11-04 18:35:02 -0500

    added resnet18 with dropout

--- dreamsdm/models.py
+++ dreamsdm/models.py
@@ -82,3 +82,46 @@ class ResNet18(nn.Module):
         out = out.view(out.size(0), -1)
         out = self.fc(out)
         return out
+
+class ResNet18Dropout(nn.Module):
+    def __init__(self, img_dim = (1, 512, 512), output_size =1):
+        super(ResNet18Dropout, self).__init__()
+        self.in_channels = 64
+        self.conv1 = nn.Conv2d(img_dim[0], 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        
+        self.layer1 = self._make_layer(BasicBlock, 64, 2, stride=1)
+        self.layer2 = self._make_layer(BasicBlock, 128, 2, stride=2)
+        self.layer3 = self._make_layer(BasicBlock, 256, 2, stride=2)
+        self.layer4 = self._make_layer(BasicBlock, 512, 2, stride=2)
+        
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, output_size)
+        self.dropout = nn.Dropout(p = 0.5)
+
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.maxpool(out)
+        
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        
+        out = self.avgpool(out)
+        out = out.view(out.size(0), -1)
+        out = self.dropout(out)
+        out = self.fc(out)
+        return out
